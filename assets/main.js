742a3c73aea1395aa5907e1cca8c2d1b802e4875
Ali Rashidnahal <alirashidnahal@yahoo.com>
2025-05-08 02:25:19 +0330

    some refactor and cleaning code

--- assets/main.js
+++ assets/main.js
@@ -1,19 +1,22 @@
-'use strict'; 
+'use strict';
 
-	var raidCalc = document.getElementById('raid-calculator');
+let raidCalc = document.getElementById('raid-calculator');
 
-	if(raidCalc) {
-		var disksNumber = raidCalc.querySelector('#numberOfDisks'),
-			diskSize = raidCalc.querySelector('#sizeOfEachDrive'),
-			availableSpace = raidCalc.querySelector('#availableSpace'),
-			totalSpace = raidCalc.querySelector('#totalSpace'),
-			raidOptions = raidCalc.querySelectorAll("input[name=raidConf]");
+let availableSpace = raidCalc.querySelector('#availableSpace');
+let totalSpace = raidCalc.querySelector('#totalSpace');
+let raidOptions = raidCalc.querySelectorAll("input[name=raidConf]");
+let dNum;
+let dSize;
+let raid;
+if(raidCalc) {
+  let disksNumber = raidCalc.querySelector('#numberOfDisks'),
+    diskSize = raidCalc.querySelector('#sizeOfEachDrive');
 
-		var dNum, dSize, raid = 0, tSpace;
+  raid = 0;
 
-		disksNumber.addEventListener('keyup', function () {
+  disksNumber.addEventListener('keyup', function () {
 			if(this.value > 0) {
-				dNum = this.value; 
+				dNum = this.value;
 				checkRaidOptions();
 				updateTotal();
 			}
@@ -21,14 +24,14 @@
 
 		diskSize.addEventListener('keyup', function () {
 			if(this.value > 0) {
-				dSize = this.value; 
+				dSize = this.value;
 				updateTotal();
 			}
 		});
 
 		disksNumber.addEventListener('change', function () {
 			if(this.value > 0) {
-				dNum = this.value; 
+				dNum = this.value;
 				checkRaidOptions();
 				updateTotal();
 			}
@@ -36,12 +39,12 @@
 
 		diskSize.addEventListener('change', function () {
 			if(this.value > 0) {
-				dSize = this.value; 
+				dSize = this.value;
 				updateTotal();
 			}
 		});
 
-		for( var i = 0; i < raidOptions.length; i++ ) {
+		for(let i = 0; i < raidOptions.length; i++ ) {
 			raidOptions[i].addEventListener('change', function() {
 				unsetSelectedRaidConfig();
 				raid = this.value;
@@ -53,34 +56,34 @@
 	}
 
 	function unsetSelectedRaidConfig () {
-		for( var i = 0; i < raidOptions.length; i++ ) {
+		for(let i = 0; i < raidOptions.length; i++ ) {
 			raidOptions[i].parentNode.classList.remove('selected');
 		}
 	}
 
 	function checkRaidOptions () {
-		
+
 		disableRaid();
 
-		if(dNum == 1) {
+		if(dNum === 1) {
 			enableRaidOption([0]);
-		} 
+		}
 
-		if(dNum == 2) {
+		if(dNum === 2) {
 			enableRaidOption([0, 1]);
-		} 
+		}
 
 		if(dNum !== 2 && dNum >= 3) {
 			enableRaidOption([0, 5]);
 		}
 
 		if(dNum >= 4) {
-			if(dNum%2 == 0) enableRaidOption([6, 10]);
-			if(dNum%2 == 1) enableRaidOption([6]);
+			if(dNum%2 === 0) enableRaidOption([6, 10]);
+			if(dNum%2 === 1) enableRaidOption([6]);
 		}
 
 		if(dNum >= 5 && dNum > 4) {
-			if(dNum%2 == 0) enableRaidOption([0, 5, 6]);
+			if(dNum%2 === 0) enableRaidOption([0, 5, 6]);
 		}
 
 		if(dNum >= 6) {
@@ -94,13 +97,13 @@
 	}
 
 	function enableRaidOption (list) {
-		for(var i = 0; i <= list.length; i++ ) {
+		for(let i = 0; i <= list.length; i++ ) {
 			enableRaidOptionByValue(list[i]);
 		}
 	}
 
 	function enableRaidOptionByValue (val) {
-		for( var i = 0; i < raidOptions.length; i++ ) {
+		for(let i = 0; i < raidOptions.length; i++ ) {
 			if(raidOptions[i].value == val)
 				enableRaid(raidOptions[i]);
 		}
@@ -112,18 +115,18 @@
 	}
 
 	function disableRaid (el) {
-		for( var i = 0; i < raidOptions.length; i++ ) {
+		for(let i = 0; i < raidOptions.length; i++ ) {
 			raidOptions[i].parentNode.classList.add('inactive');
 			raidOptions[i].setAttribute('disabled', 'disabled');
 		}
 	}
 
 	function updateBar (total) {
-		var availableSpace = dNum * dSize;
-		var percent = (availableSpace/100);
-		var availablePercent = parseInt(total/percent); 
+    let availableSpace = dNum * dSize;
+    let percent = (availableSpace / 100);
+    let availablePercent = parseInt(total / percent);
 
-		raidCalc.querySelector('.progress-bar').setAttribute('style', 'width: ' + availablePercent + '%');
+    raidCalc.querySelector('.progress-bar').setAttribute('style', 'width: ' + availablePercent + '%');
 	}
 
 	function updateTotal () {
@@ -133,9 +136,9 @@
 
 		if( dNum > 0 && dSize > 0 && raid >= 0 ) {
 
-			var total = -1;
+      let total = -1;
 
-			if(raid === 0 && dNum >= 1)
+      if(raid === 0 && dNum >= 1)
 				total = dNum * dSize;
 
 			if(raid === 1 && dNum === 2)
